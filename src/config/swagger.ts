3e4330fbbdce1c040ad2c78044ce52d4a71e873c
lamjed gaidi <lamjed.gaidi@tech-expert.io>
2025-01-02 11:38:06 +0100

    saving : fix swagger problems

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -14,7 +14,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://express-horoscope-app.onrender.com",
         description: "Local Development Server",
       },
     ],
