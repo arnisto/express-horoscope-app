5cbdec924484f3023a2ed5e4fc312bb95a0e2c52
lamjed gaidi <lamjed.gaidi@tech-expert.io>
2025-01-02 11:40:51 +0100

    saving : update console.log

--- src/index.ts
+++ src/index.ts
@@ -59,5 +59,7 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/v1/horoscope", horoscopeRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(
+    `Server is running on https://express-horoscope-app.onrender.com/:${PORT}`
+  );
 });
